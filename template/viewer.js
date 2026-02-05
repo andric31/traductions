c016819478c1c125b90a3482e2e7af0d12c60f01
andric31 <andric31@gmail.com>
2026-02-05 19:02:18 +0100

    Add files via upload

--- template/viewer.js
+++ template/viewer.js
@@ -63,7 +63,7 @@
       aHome.href = "https://traductions.pages.dev/";
       aHome.target = "_self";
       aHome.rel = "noopener";
-      aHome.textContent = "🏠 Accueil général";
+      aHome.textContent = "🌍 Projet Traductions FR";
       aHome.style.display = "block";
       aHome.style.textDecoration = "none";
       pop.appendChild(aHome);
