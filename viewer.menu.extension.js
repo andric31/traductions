2940ef0750c657123bc517a6f59cd5ba38bfadb3
andric31 <andric31@gmail.com>
2026-02-20 18:38:04 +0100

    Add files via upload

--- viewer.menu.extension.js
+++ viewer.menu.extension.js
@@ -93,6 +93,8 @@ C’est simple, rapide, et super pratique pour suivre mes trads sans te perdre !
 
         <div style="text-align:center;opacity:.95;margin-bottom:12px;">
           ${escapeHtml("Voici mon extension qui ajoute une icône directement sur les threads et les vignettes de F95Zone.")}
+          <br><br>
+          ${escapeHtml("L’icône est cliquable et permet d’accéder directement aux informations de la traduction (Discord, version, mise à jour, etc.).")}
         </div>
 
         ${imageBlock(IMAGES[0])}
